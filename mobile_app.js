a55b476180684af2551066d27ca50ec40a3a78ff
Cunningham-Ian <ianhewson222@gmail.com>
2017-07-18 22:29:18 -0600

    a

--- mobile_app.js
+++ mobile_app.js
@@ -9,11 +9,12 @@ document.getElementById("btn1").addEventListener("click", function() {
 	var location = document.getElementById("location");
 	var txt = document.createElement("LABEL");
 	input.setAttribute("id", "input1");
-	var btn = document.createElement("BTN");
-	var btnTxt = document.createTextNode("Submit");
-	btn.appendChild(btnTxt);
-	btn.setAttribute("type", "button");
-	btn.setAttribute("id", "btn2");
+//	var btn = document.createElement("BTN");
+//	var btnTxt = document.createTextNode("Submit");
+//	btn.appendChild(btnTxt);
+//	btn.setAttribute("type", "button");
+//	btn.setAttribute("id", "btn2");
+	document.getElementById("instruct").innerHTML = '(Type the location, then hit "Submit")';
 	if (document.getElementById("lat,lon").checked) {
 		txt.innerHTML = "lat: ";
 		location.appendChild(txt);
@@ -25,7 +26,6 @@ document.getElementById("btn1").addEventListener("click", function() {
 		txt2.innerHTML = "lon: ";
 		location.appendChild(txt2);
 		document.getElementById("location").appendChild(input2);
-		location.appendChild(btn);
 	} else if (document.getElementById("city").checked) {
 		var input2 = document.createElement("INPUT");
 		input2.setAttribute("id", "input2");
@@ -37,12 +37,10 @@ document.getElementById("btn1").addEventListener("click", function() {
 		txt2.innerHTML = "state/country: ";
 		location.appendChild(txt2);
 		document.getElementById("location").appendChild(input2);
-		location.appendChild(btn);
 	} else {
 		txt.innerHTML = "Type in the appropriate text: ";
 		location.appendChild(txt);
 		location.appendChild(input);
-		location.appendChild(btn);
 	}
 });
 });
@@ -50,7 +48,6 @@ if(document.getElementById("btn2")) {
 document.getElementById("btn2").addEventListener("click", function () {
 	document.getElementById("location").style.display = "none";
 	document.getElementById("overallData").style.display = "block";
-	document.getElementById("question2").style.transition = "recolor2 1s";
 	//				window.alert("2nd function");
 	localStorage.setItem("input1", document.getElementById("input1").value);
 	if (document.getElementById("input2")) {
@@ -85,21 +82,7 @@ document.getElementById("btn3").addEventListener("click", function () {
 
 function getData(type) {
 	if (typeof (Storage) !== "undefined") {
-		function parse() {
-		document.getElementById("overallData").style.display = "none";
-				if	(type == "conditions") {
-					document.getElementById("conditions").style.display = "block";
-					document.getElementById("")
-					
-				} else if (type == "forecast") {
-				document.getElementById("forecast").style.display = "block";
-				
-			} else {
-				document.getElementById("hourly").style.display = "block";
-				
-			}
-			}
-	}
+		
 			if	(localStorage.getItem("data_type") == "conditions") {
 				var api = "conditions";
 			} else if (localStorage.getItem("data_type") == "forecast") {
@@ -116,6 +99,66 @@ function getData(type) {
 			}
 			document.getElementById("jsonScr").setAttribute("src", "https://api.wunderground.com/api/0374d8d7218313b5/geolookup/" + api + "/q/" + place + ".json?callback=parse");
 		}
+		
+	parse = function(data){
+		console.log(data);
+		var type = localStorage.getItem("data_type");
+		if (type == "hourly") {
+			document.getElementById("hourly").style.display = "block";
+			var newTr = document.createElement("TR");
+			for (var i = 0; i < 3; i++) {
+				var newTh = document.createElement("TH");
+				var hr = document.createTextNode("Hour: " + data.hourly_forecast[i].FCTTIME.hour + " ");
+				newTh.appendChild(hr);
+				newTr.appendChild(newTh);
+				// Change style width and border to make it look nicer
+			}
+			document.getElementById("hourly").appendChild(newTr);
+			var secondTr = document.createElement("TR");
+			for (var i = 0; i < 3; i++) {
+				var condTd = document.createElement("TD");
+				var cond = document.createTextNode("Condition: " + data.hourly_forecast[i].condition + " ");
+				console.log(cond);
+				condTd.appendChild(cond);
+				secondTr.appendChild(condTd);
+				// Change style width and border to make it look nicer
+			}
+			document.getElementById("hourly").appendChild(secondTr);
+			var thirdTr = document.createElement("TR");
+			for (var i = 0; i < 3; i++) {
+				var forecastTd = document.createElement("TD");
+				var pic = document.createElement("IMG");
+				pic.setAttribute("SRC", data[i].hourly_forecast[i].icon_url);
+				forecastTd.appendChild(pic);
+				thirdTr.appendChild(forecastTd);
+			}
+			document.getElementById(thirdTr);
+		} else if (type == "conditions") {
+			document.getElementById("conditions").style.display = "block";
+			document.getElementById("curr_con").innerHTML = data.forecast.forecastday[0].icon;
+			document.getElementById("currConPic").setAttribute("src", data.forecast.forecastday[0].icon_url);
+		} else {
+			document.getElementById("forecast").style.display = "block";
+				for (var i = 0; i < 4; i+=2) {
+				var newTh = document.createElement("TH");
+				var day = document.createTextNode(data.forecast.txt_forecast.forecastday[i].title + " ");
+				newTh.appendChild(day);
+				newTr.appendChild(newTh);
+				// Change style width and border to make it look nicer
+			}
+				document.getElementById("forecast").appendChild(newTr);
+				var secondTr = document.createElement("TR");
+			for (var i = 0; i < 4; i+=2) {
+				var newTd = document.createElement("TD");
+				var elem = document.createElement("IMG"); 
+				newTh.appendChild(day);
+				newTr.appendChild(newTh);
+				// Change style width and border to make it look nicer
+			}
+			var thirdTr = document.createElement("TR");
+			}
+		};
+	}
 		/*
 			If #data_type is conditions,
 				api = conditions
@@ -140,11 +183,9 @@ function getData(type) {
 			If data type is hourly,
 			Similar to forecast
 		*/
-		
-		xhr.send();
 		// A function for changing a string to TitleCase
-	function toTitleCase(str) {
-		return str.replace(/\w+/g, function (txt) {
-			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		});
-	}
+//	function toTitleCase(str) {
+//		return str.replace(/\w+/g, function (txt) {
+//			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//		});
+//	}
